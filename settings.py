c77a3948931d8b8da06db0e995337f77917facad
KRamos21 <kaue.ramos.krr@gmail.com>
2020-06-01 15:25:57 -0300

    Criei a nave espacial

--- settings.py
+++ settings.py
@@ -4,6 +4,6 @@ class Settings:
     def __init__(self):
         """Initialize the game settings."""
         # Screen settings.
-        self.screen_width = 1920
-        self.screen_height = 1080
-        self.bg_color = (203, 230, 230)
+        self.screen_width = 1200
+        self.screen_height = 800
+        self.bg_color = (230, 230, 230)
