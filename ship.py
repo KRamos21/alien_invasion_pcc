99ff27e34607dce80d67b69038ddf3113a290154
KRamos21 <kaue.ramos.krr@gmail.com>
2020-06-02 16:25:56 -0300

    Criei a classe Alien e a fiz ser carregada no jogo

--- ship.py
+++ ship.py
@@ -5,7 +5,7 @@ class Ship:
     """A class to manage the ship."""
 
     def __init__(self, ai_game):
-        """Initialize the shio and set its starting position."""
+        """Initialize the ship and set its starting position."""
         self.screen = ai_game.screen
         self.settings = ai_game.settings
         self.screen_rect = ai_game.screen.get_rect()
